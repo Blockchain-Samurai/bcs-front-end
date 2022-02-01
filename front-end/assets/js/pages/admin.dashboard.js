ad345e08edd0a7a3d7fca3d8e443bb765b75599b
Casey Mershon <caseymershon@gmail.com>
2022-02-01 01:16:13 -0500

    wider datatable & edit option

--- front-end/assets/js/pages/admin.dashboard.js
+++ front-end/assets/js/pages/admin.dashboard.js
@@ -21,32 +21,31 @@ async function buildSamurai(){
     const samuraiTable = $('#samurai-table');
     const samuraiTableBody = $('#samurai-table-body');
     await getAllSamurai(0).then(async res => {
-        console.log('get init: ', res);
         let samuraiTotal = parseInt(res.samurai_data_total.total);
         let samuraiPages = Math.ceil(samuraiTotal/25) - 1;
-        console.log('total: ', samuraiTotal);
-        console.log('pages: ', samuraiPages);
 
         for(let i = 0; i <= samuraiPages; i++){
             await getAllSamurai(i).then(async res => {
-                console.log("Get Samurai Page: ", i);
                 res.data.forEach(s => {
-                    console.log("Current Samurai: ", s);
                     let currRow = `
                     <tr>
-                        <td class="text-truncate" style="max-width: 150px;">${s.id}</td>
-                        <td class="text-center"><img src="${s.image}" class="img-rounded" alt="Cinque Terre"></td>
+                        <td class="text-center">${s.id}</td>
+                        <td class="text-center"><img src="${s.image}" class="rounded-circle" style="max-height: 45px; max-width: 45px;"></td>
                         <td class="text-center">${s.name}</td>
                         <td class="text-center">${s.description}</td>
                         <td class="text-center">${s.rarity}</td>
                         <td class="text-center">${s.clan.name}</td>
                         <td class="text-center">${s.user.tag}</td>
                         <td>
-                            <button id="samurai-delete-button-${s.id}" class="btn btn-danger btn-icon-split btn-sm" type="button" data-toggle="modal" data-target="#samurai_modal_${s.id}" data-backdrop="static" data-keyboard="false">
+                            <button id="samurai-delete-button-${s.id}" class="btn btn-danger btn-sm" type="button" data-toggle="modal" data-target="#samurai_delete_modal_${s.id}" data-backdrop="static" data-keyboard="false">
                                 <span class="icon text-white-50">
                                     <i class="fas fa-trash-alt"></i>
                                 </span>
-                                <span class="text">Delete</span>
+                            </button>
+                            <button id="samurai-delete-button-${s.id}" class="btn btn-info btn-sm" type="button" data-toggle="modal" data-target="#samurai_edit_modal_${s.id}" data-backdrop="static" data-keyboard="false">
+                                <span class="icon text-white-50">
+                                    <i class="fas fa-edit"></i>
+                                </span>
                             </button>
                         </td>
                     </tr>
@@ -54,7 +53,6 @@ async function buildSamurai(){
                     samuraiTableBody.append(currRow);
                 });
             });
-            console.log("Build data table...")
             samuraiTable.DataTable({
                 "columnDefs": [{
                     "targets": 7,
